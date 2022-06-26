ee631ee06c1c109af1e3958dac1cd059054f9805
lucazulian <lucagiuggia@gmail.com>
2022-06-26 22:41:26 +0200

    Some optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -25,80 +25,6 @@ use ehal::blocking::i2c::{Read, Write, WriteRead};
 const WRITE_ADDRESS: u8 = 0x80;
 const READ_ADDRESS: u8 = WRITE_ADDRESS >> 1;
 
-/// Struct for GP2Y0E02B
-#[derive(Debug, Copy, Clone)]
-pub struct GP2Y0E02B<I2C> {
-    com: I2C,
-}
-
-/// Defines errors
-#[derive(Debug, Copy, Clone)]
-pub enum Error<E> {
-    /// Underlying bus error
-    BusError(E),
-    /// Timeout
-    Timeout,
-}
-
-impl<E> From<E> for Error<E> {
-    fn from(error: E) -> Self {
-        Error::BusError(error)
-    }
-}
-
-impl<I2C, E> GP2Y0E02B<I2C>
-where
-    I2C: Write<Error = E> + Read<Error = E> + WriteRead<Error = E>,
-    E: core::fmt::Debug,
-{
-    /// Creates a new sensor
-    pub fn new(i2c: I2C) -> Result<GP2Y0E02B<I2C>, Error<E>> {
-        let chip = GP2Y0E02B { com: i2c };
-        Ok(chip)
-    }
-
-    /// TODO
-    pub fn read_register(&mut self, reg: Register) -> Result<u8, E> {
-        self.read_byte(reg as u8)
-    }
-
-    /// TODO
-    pub fn write_register(&mut self, reg: Register, byte: u8) -> Result<(), E> {
-        self.write_byte(reg as u8, byte)
-    }
-
-    /// Reads and returns distance measurement in millimeters
-    pub fn read_distance(&mut self) -> Result<f32, E> {
-        let high = self.read_byte(0x5E).unwrap() as f32;
-        let low = self.read_byte(0x5F).unwrap() as f32;
-        let shift = self.read_byte(0x35).unwrap() as u32;
-
-        let v = (((high * 16.0) + low) / 16.0) / (u16::pow(2, shift) as f32);
-
-        Ok(v)
-    }
-
-    fn read_byte(&mut self, reg: u8) -> Result<u8, E> {
-        let mut data: [u8; 1] = [0];
-        self.com.write_read(READ_ADDRESS, &[reg], &mut data)?;
-
-        Ok(data[0])
-    }
-
-    fn read_bytes(&mut self, reg: u8) -> Result<[u8; 2], E> {
-        let mut data: [u8; 2] = [0, 0];
-        self.com.write_read(READ_ADDRESS, &[reg], &mut data)?;
-
-        Ok(data)
-    }
-
-    fn write_byte(&mut self, reg: u8, byte: u8) -> Result<(), E> {
-        let mut buffer = [0];
-        self.com
-            .write_read(WRITE_ADDRESS, &[reg, byte], &mut buffer)
-    }
-}
-
 /// Register Map (Bank0) from <http://www.robot-electronics.co.uk/files/gp2y0e02_03_appl_e.pdf>
 #[allow(non_camel_case_types)]
 #[derive(Copy, Clone, Debug)]
@@ -287,4 +213,71 @@ pub enum Register {
     /// Spot Symmetry=|(C－A)－2*B|
     /// (=|(0xF8\[7:0\]－0xF9\[7:0\])－2*0xFA\[7:0\])|)
     PEAK_COORDINATE = 0xFA,
+}
+
+impl Register {
+    /// Explicitly get address of a Register
+    #[inline(always)]
+    pub fn addr(self) -> u8 {
+        self as u8
+    }
+}
+
+/// Struct for GP2Y0E02B
+#[derive(Debug, Copy, Clone)]
+pub struct GP2Y0E02B<I2C> {
+    com: I2C,
+}
+
+impl<I2C, E> GP2Y0E02B<I2C>
+where
+    I2C: Write<Error = E> + Read<Error = E> + WriteRead<Error = E>,
+    E: core::fmt::Debug,
+{
+    /// Creates a new sensor
+    pub fn new(i2c: I2C) -> Result<GP2Y0E02B<I2C>, E> {
+        let chip = GP2Y0E02B { com: i2c };
+        Ok(chip)
+    }
+
+    /// TODO
+    pub fn read_register(&mut self, reg: Register) -> Result<u8, E> {
+        self.read_byte(reg as u8)
+    }
+
+    /// TODO
+    pub fn write_register(&mut self, reg: Register, byte: u8) -> Result<(), E> {
+        self.write_byte(reg as u8, byte)
+    }
+
+    /// Reads and returns distance measurement in millimeters
+    pub fn read_distance(&mut self) -> Result<f32, E> {
+        let high = self.read_byte(Register::DISTANCE_11_4.addr()).unwrap() as f32;
+        let low = self.read_byte(Register::DISTANCE_3_0.addr()).unwrap() as f32;
+        let shift = self.read_byte(Register::SHIFT_BIT.addr()).unwrap() as u32;
+
+        let v = (((high * 16.0) + low) / 16.0) / (u16::pow(2, shift) as f32);
+
+        Ok(v)
+    }
+
+    fn read_byte(&mut self, reg: u8) -> Result<u8, E> {
+        let mut data: [u8; 1] = [0];
+        self.com.write_read(READ_ADDRESS, &[reg], &mut data)?;
+
+        Ok(data[0])
+    }
+
+    fn read_bytes(&mut self, reg: u8) -> Result<[u8; 2], E> {
+        let mut data: [u8; 2] = [0, 0];
+        self.com.write_read(READ_ADDRESS, &[reg], &mut data)?;
+
+        Ok(data)
+    }
+
+    fn write_byte(&mut self, reg: u8, byte: u8) -> Result<(), E> {
+        let mut buffer = [0];
+        self.com
+            .write_read(WRITE_ADDRESS, &[reg, byte], &mut buffer)
+    }
 }
